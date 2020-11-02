8d3e7a99d5cd36832c1f2b76cfc2ce11dfb77909
Gabriel Ramos <gabrielsa@ufcspa.edu.br>
2020-11-02 15:06:37 +0000

    definição de rotas

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 
 var app = express();
 
-mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true});
+mongoose.connect('mongodb://localhost/encomendas', {useNewUrlParser: true});
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
@@ -11,9 +11,34 @@ db.once('open', function() {
   console.log('logado');
 });
 
+var consultarEncomenda = require('./routes/consultarEncomenda');
+var cadastrarEncomenda = require('./routes/cadastrarEncomenda');
+var atualizarStatusEncomenda = require('./routes/atualizarStatusEncomenda');
+
+
 
 app.get('/', (req, res) => {
     res.send('Hello World');
+})
+
+app.get('/encomenda/:codigo', (req, res) => {
+  var codigo = req.params.codigo;
+  consultarEncomenda(codigo, res);
+})
+
+app.post('/encomenda/:nome/:codigo', (req,res) => {
+  var nome = req.params.nome;
+  var codigo = req.params.codigo;
+  cadastrarEncomenda({nome: nome, codigo: codigo}, res);
+})
+
+app.post('/encomenda/status/:codigo/:status', (req, res) => {
+  var codigo = req.params.codigo;
+  var status = req.params.status;
+  if(status > 4 || status < 1)
+    res.status(500).send({message: "Status inválido, utlize valores entre 1 e 4"});
+  else
+    atualizarStatusEncomenda({codigo: codigo, status: status}, res);
 })
 
 app.listen(3000, () => {
